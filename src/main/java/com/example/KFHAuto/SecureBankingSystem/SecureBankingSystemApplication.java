4f39427253c2d0b5fe8bf82b5d62accf512249c6
aebinessa <abinessa0004@sdsu.edu>
2024-02-12 09:29:03 +0300

    adds files

---
+++ src/main/java/com/example/KFHAuto/SecureBankingSystem/SecureBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.KFHAuto.SecureBankingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KFHAuto/SecureBankingSystem/SecureBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KFHAuto.SecureBankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
